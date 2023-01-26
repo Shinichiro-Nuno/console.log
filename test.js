d487083d34b7b0a0be77d848efaa9cb51c3ce3fe
Shinichiro <nuno1216shin@gmail.com>
2023-01-26 22:39:15 +0900

    配列　オブジェクト

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
-// for(i = 0; i < 10; i++) {
-//   console.log(i);
-// }
+const x = ["orange", "apple", "banana"]
+console.log(x);
+console.log(x[2]);
 
-let x = 0;
-while(x < 10) {
-  console.log(x);
-  ++x;
+const poti = {
+  age: 2,
+  weight: "10キロ",
+  type: "dog"
 }
+
+console.log(poti);
+console.log(poti.age);
+console.log(poti.weight);
